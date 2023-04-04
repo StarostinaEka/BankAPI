52c8a1d8692a5b8d591609bba244873006bc63e0
Eka Starostina <starostinaeka@gmail.com>
2023-04-04 14:16:45 +0300

    JSON modified1

--- api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ api-ci/rest/src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,6 +4,7 @@ import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
     @Test
@@ -17,6 +18,22 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                ;
+    }
+    @Test
+    void shouldReturnCurrencies() {
+        // Given - When - Then
+        // Предусловия
+        ValidatableResponse body = given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("it.currency", "enum": ["RUB", "USD"]);
+
     }
 }
